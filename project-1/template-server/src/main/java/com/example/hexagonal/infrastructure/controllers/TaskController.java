40d75c0035eca88e83a93b9d44163e1bb3e93e14
JuanC <abjuancarlos.12@gmail.com>
2024-06-18 01:59:57 -0500

    chore: created driver to test functionality of creating a task

---
+++ project-1/template-server/src/main/java/com/example/hexagonal/infrastructure/controllers/TaskController.java
@@ -0,0 +1,28 @@
+package com.example.hexagonal.infrastructure.controllers;
+
+import com.example.hexagonal.appication.services.TaskServices;
+import com.example.hexagonal.domain.models.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/task/")
+public class TaskController {
+
+    private final TaskServices taskService;
+
+    public TaskController(TaskServices taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+
+}
